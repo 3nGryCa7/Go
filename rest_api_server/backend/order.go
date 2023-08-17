5e200b5f34cb2b77df7f2113ccce3d4fd1f53d51
3nGryCa7 <82641653+3nGryCa7@users.noreply.github.com>
2023-08-17 17:17:25 +0800

    rest_api_server
    product/order, frontend and backend

---
+++ rest_api_server/backend/order.go
@@ -0,0 +1,95 @@
+package backend
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type order struct {
+	ID           int         `json:"id"`
+	CustomerName string      `json:"customerName"`
+	Total        int         `json:"totalPrice"`
+	Status       string      `json:"status"`
+	Items        []orderItem `json:"items"`
+}
+
+type orderItem struct {
+	OrderID   int `json:"orderId"`
+	ProductID int `json:"productId"`
+	Quantity  int `json:"quantity"`
+}
+
+func getOrders(db *sql.DB) ([]order, error) {
+	rows, err := db.Query("SELECT * FROM orders")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	orders := []order{}
+
+	for rows.Next() {
+		var o order
+		if err := rows.Scan(&o.ID, &o.CustomerName, &o.Total, &o.Status); err != nil {
+			return nil, err
+		}
+		if err := o.getOrderItems(db); err != nil {
+			return nil, err
+		}
+		orders = append(orders, o)
+	}
+	return orders, err
+}
+
+func (o *order) getOrder(db *sql.DB) error {
+	db.QueryRow("SELECT customerName, total, status FROM orders WHERE id=?", o.ID).Scan(&o.CustomerName, &o.Total, &o.Status)
+	err := o.getOrderItems(db)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *order) getOrderItems(db *sql.DB) error {
+	rows, err := db.Query("SELECT * FROM orderItems WHERE orderId=?", o.ID)
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+	orderItems := []orderItem{}
+
+	for rows.Next() {
+		var oi orderItem
+		if err := rows.Scan(&oi.OrderID, &oi.ProductID, &oi.Quantity); err != nil {
+			return err
+		}
+		orderItems = append(orderItems, oi)
+	}
+	o.Items = orderItems
+	return nil
+}
+
+func (o *order) createOrder(db *sql.DB) error {
+	res, err := db.Exec("INSERT INTO orders(customerName, total, status) VALUES(?, ?, ?)", o.CustomerName, o.Total, o.Status)
+	if err != nil {
+		return err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+	o.ID = int(id)
+	return nil
+}
+
+func (oi *orderItem) createOrderItem(db *sql.DB) error {
+	_, err := db.Exec("INSERT INTO orderItems(orderId, productId, quantity) VALUES(?, ?, ?)", oi.OrderID, oi.ProductID, oi.Quantity)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ rest_api_server/frontend/frontend.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"example.com/backend"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	a := backend.App{}
+	a.Port = ":8080"
+	a.Initailize()
+	a.Run()
+}
